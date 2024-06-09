971e9c3a018f58383ed563f377be53b2a33ac34d
islerdev <38693883+islerdev@users.noreply.github.com>
2024-06-09 16:36:45 +0300

    Update Main.java

--- day4Hw2/src/day4Hw2/Main.java
+++ day4Hw2/src/day4Hw2/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
-		customerManager.save(new Customer(1,"Engin","Demiroð",new Date(1985,1,1), "11111111111"));		
+		customerManager.save(new Customer(1,"Salih","Demir",new Date(1985,1,1), "11111111111"));		
 		
 		
 		
